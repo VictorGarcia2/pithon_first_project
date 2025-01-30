4f2dd8008bf095e7afc7d0597bcd124a8ce1d966
VictorGarcia2 <victrgarciaphoto@gmail.com>
2025-01-30 08:51:23 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+
+nombre = int(input("Introduce un numero: "))
+# una variable solamente se especifica el nombre de la variable
+# el input me permite solicitar los datos al usuario. Siempre retorna string
+#Tipps de dato: Int, Float, Str, List, Boolean, Tuplas, Set, Dict
+
+miLista = [1,2,3,4,5,6]
+miDict = {
+    "llave": "valor",
+    "apellido" : "cruz",
+    }
+
+
+
+print(type(miLista))
+print(type(miDict))
